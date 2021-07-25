d85bced63b43179cd7e83a2c180c198b4d58c81e
parksil0 <parksil00@naver.com>
2021-07-26 07:53:51 +0900

    초기설정

--- frontend/pages/Channel/index.tsx
+++ frontend/pages/Channel/index.tsx
@@ -1,11 +1,12 @@
 import Workspace from '@layouts/Workspace';
 import React from 'react';
+import { Container, Header } from './styles';
 
 const Channel = () => {
   return (
-    <Workspace>
-      <div>로그인 추카!</div>
-    </Workspace>
+    <Container>
+      <Header>채널</Header>
+    </Container>
   )
 }
 
