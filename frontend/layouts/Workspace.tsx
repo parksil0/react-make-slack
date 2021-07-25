5240a9ac0ffb5f8c470713535ec11379b67bfee2
parksil0 <parksil00@naver.com>
2021-07-25 23:48:20 +0900

    레이아웃 추가(children 설정)

---
+++ frontend/layouts/Workspace.tsx
@@ -0,0 +1,31 @@
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React, { FC, useCallback } from 'react';
+import { Redirect } from 'react-router';
+import useSWR from 'swr';
+
+const Workspace: FC = ({ children }) => {
+  const { data, error, revalidate, mutate } = useSWR('/api/users', fetcher);
+
+  const onLogOut = useCallback(() => {
+    axios.post('/api/users/logout', null, {
+      withCredentials: true,
+    })
+    .then(() => {
+      revalidate();
+    })
+  }, []);
+
+  if (!data) {
+    return <Redirect to="/login" />
+  }
+  
+  return (
+    <div>
+      <button onClick={onLogOut}>로그아웃</button>
+      {children};
+    </div>
+  )
+}
+
+export default Workspace;
