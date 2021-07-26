e864c44a53648bfd3895298352fd42c5c17f6dc8
parksil0 <parksil00@naver.com>
2021-07-26 23:55:32 +0900

    채널 생성 모달 컴포넌트 추가 컴포넌트 분리를 하면 화면의 렌더가 적어진다

---
+++ frontend/components/CreateChannelModal/index.tsx
@@ -0,0 +1,31 @@
+import Modal from '@components/Modal';
+import useInput from '@hooks/useInput';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import React, { FC, useCallback } from 'react';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+}
+
+const CreateChannelModal: FC<Props> = ({ show, onCloseModal,}) => {
+  const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
+
+  const onCreateChannel = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+
+  return (
+    <Modal show={show} onCloseModal={onCloseModal}>
+      <form onSubmit={onCreateChannel}>
+        <Label id="channel-label">
+          <span>채널 이름</span>
+          <Input id="workspace" value={newChannel} onChange={onChangeNewChannel} />
+        </Label>
+        <Button type="submit">생성하기</Button>
+      </form>
+    </Modal>
+  )
+}
+
+export default CreateChannelModal;
