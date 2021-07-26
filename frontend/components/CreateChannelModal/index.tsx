bf5a6622b92ff89623bddc24f9cf093737c9d2c3
parksil0 <parksil00@naver.com>
2021-07-27 07:28:39 +0900

    채널 생성 모달 swr 설정, 비동기 api 요청 로직 추가

--- frontend/components/CreateChannelModal/index.tsx
+++ frontend/components/CreateChannelModal/index.tsx
@@ -1,19 +1,47 @@
 import Modal from '@components/Modal';
 import useInput from '@hooks/useInput';
 import { Button, Input, Label } from '@pages/SignUp/styles';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
 import React, { FC, useCallback } from 'react';
+import { useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import useSWR from 'swr';
 
 interface Props {
   show: boolean;
   onCloseModal: () => void;
+  setShowCreateChannelModal: (flag: boolean) => void;
 }
 
-const CreateChannelModal: FC<Props> = ({ show, onCloseModal,}) => {
+const CreateChannelModal: FC<Props> = ({ show, onCloseModal, setShowCreateChannelModal, }) => {
+  const { workspace, channel } = useParams<{ workspace: string; channel: string; }>();
+
+  const { data: userData, error, revalidate, mutate } = useSWR<IUser | false>('/api/users', fetcher);
+  const { data: channelData, revalidate: revalidateChannel } = useSWR<IChannel[]>(userData ? `/api/workspaces/${workspace}/channels` : null, fetcher)
+
+  console.log(channelData, userData);
+
   const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
 
   const onCreateChannel = useCallback((e) => {
     e.preventDefault();
-  }, []);
+    axios.post(`/api/workspaces/${workspace}/channels`, {
+      name: newChannel,
+    }, {
+      withCredentials: true,
+    })
+      .then(() => {
+        setShowCreateChannelModal(false);
+        revalidateChannel();
+        setNewChannel('');
+      })
+      .catch((error) => {
+        console.dir(error);
+        toast.error(error.response?.data, { position: 'bottom-center' });
+      })
+  }, [newChannel]);
 
   return (
     <Modal show={show} onCloseModal={onCloseModal}>
