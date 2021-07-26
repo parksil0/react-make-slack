c53e85b3387b14830c05e0cf3e7ab4d71f7da16f
parksil0 <parksil00@naver.com>
2021-07-27 07:33:02 +0900

    레이아웃에 모달 추가(워크스페이스 초대, 워크스페이스 내의 채널 초대) 이외에,  - 라우트 주소 변경  - 로그인 이후에 워크스페이스가 없으면 호출하지 않도록 로직 변경  - 채널 리스트  - url파라미터 변수화

--- frontend/layouts/Workspace/index.tsx
+++ frontend/layouts/Workspace/index.tsx
@@ -1,31 +1,38 @@
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
 import React, { FC, useCallback, useState } from 'react';
-import { Redirect, Route, Switch } from 'react-router';
+import { Redirect, Route, Switch, useParams } from 'react-router';
 import useSWR from 'swr';
 import { AddButton, Channels, Chats, Header, MenuScroll, ProfileImg, ProfileModal, RightMenu, WorkspaceButton, WorkspaceModal, WorkspaceName, Workspaces, WorkspaceWrapper } from './styles';
 import gravatar from 'gravatar';
 import loadable from '@loadable/component';
 import Menu from '@components/Menu';
 import { Link } from 'react-router-dom';
-import { IUser } from '@typings/db';
+import { IChannel, IUser } from '@typings/db';
 import { Button, Input, Label } from '@pages/SignUp/styles';
 import useInput from '@hooks/useInput';
 import Modal from '@components/Modal';
 import { toast } from 'react-toastify';
 import CreateChannelModal from '@components/CreateChannelModal';
+import InviteWorkspaceModal from '@components/InviteWorkspaceModal';
+import InviteChannelModal from '@components/InviteChannelModal';
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 // children이 필요없으면 VFC
 const Workspace: FC = ({ children }) => {
+  const { workspace } = useParams<{ workspace: string; }>();
+
   const { data: userData, error, revalidate, mutate } = useSWR<IUser | false>('/api/users', fetcher);
+  const { data: channelData } = useSWR<IChannel[]>(userData ? `/api/workspaces/${workspace}/channels` : null, fetcher)
 
   const [showUserMenu, setShowUserMenu] = useState(false);
   const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] = useState(false);
   const [showWorkspaceModal, setShowWorkspaceModal] = useState(false);
   const [showCreateChannelModal, setShowCreateChannelModal] = useState(false);
+  const [showInviteWorkspaceModal, setShowInviteWorkspaceModal] = useState(false);
+  const [showInviteChannelModal, setShowInviteChannelModal] = useState(false);
 
   const [newWorkspace, onChangeNewWorkspace, setNewWorkspace] = useInput('');
   const [newUrl, onChangeNewUrl, setNewUrl] = useInput('');
@@ -78,6 +85,8 @@ const Workspace: FC = ({ children }) => {
   const onCloseModal = useCallback(() => {
     setShowCreateWorkspaceModal(false);
     setShowCreateChannelModal(false);
+    setShowInviteWorkspaceModal(false);
+    setShowInviteChannelModal(false);
   }, []);  
 
   const toggleWorkspaceModal = useCallback(() => {
@@ -87,6 +96,8 @@ const Workspace: FC = ({ children }) => {
   const onClickAddChannel = useCallback(() => {
     setShowCreateChannelModal(true);
   }, []);
+
+  const onClickInviteWorkspace = useCallback(() => {}, []);
 
   if (!userData) {
     return <Redirect to="/login" />
@@ -115,7 +126,7 @@ const Workspace: FC = ({ children }) => {
       </Header>
       <WorkspaceWrapper>
         <Workspaces>
-          {userData.Workspaces.map((ws) => {
+          {userData.Workspaces?.map((ws) => {
             return (
               <Link key={ws.id} to={`/workspace/${123}/channel/일반`}>
                 <WorkspaceButton>{ws.name.slice(0, 1).toUpperCase()}</WorkspaceButton>
@@ -132,17 +143,21 @@ const Workspace: FC = ({ children }) => {
             <Menu show={showWorkspaceModal} onCloseModal={toggleWorkspaceModal} style={{ top: 95, left: 80 }}>
               <WorkspaceModal>
                 <h2>Sleact</h2>
+                <button onClick={onClickInviteWorkspace}>워크스페이스에 사용자 초대</button>
                 <button onClick={onClickAddChannel}>채널 만들기</button>
                 <button onClick={onLogOut}>로그아웃</button>
               </WorkspaceModal>
             </Menu>
+            {channelData?.map((v) => (
+              <div>{v.name}</div>
+            ))}
           </MenuScroll>
         </Channels>
         <Chats>
           <Switch>
             {/* nested route : 라우트 계층구조가 확실해야 한다. */}
-            <Route path='/workspace/channel' component={ Channel } />
-            <Route path='/workspace/dm' component={ DirectMessage } />
+            <Route path='/workspace/:workspace/channel/:channel' component={ Channel } />
+            <Route path='/workspace/:workspace/dm/:id' component={ DirectMessage } />
           </Switch>
         </Chats>
 
@@ -163,7 +178,10 @@ const Workspace: FC = ({ children }) => {
       </Modal>
       <CreateChannelModal 
         show={showCreateChannelModal} 
-        onCloseModal={onCloseModal} />
+        onCloseModal={onCloseModal} 
+        setShowCreateChannelModal={setShowCreateChannelModal}/>
+      <InviteWorkspaceModal show={showInviteWorkspaceModal} onCloseModal={onCloseModal} setShowInviteWorkspaceModal={setShowInviteWorkspaceModal} />
+      <InviteChannelModal show={showInviteChannelModal} onCloseModal={onCloseModal} setShowInviteChannelModal={setShowInviteChannelModal} />
     </div>
   )
 }
