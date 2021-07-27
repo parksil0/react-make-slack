2bbcbe7f1a36ed891aa3210056227554d40c9ef1
parksil0 <parksil00@naver.com>
2021-07-28 07:11:02 +0900

    레이아웃 기준 우측 채팅창 페이지 설정

--- frontend/pages/DirectMessage/index.tsx
+++ frontend/pages/DirectMessage/index.tsx
@@ -1,9 +1,58 @@
 // import Workspace from '@layouts/Workspace';
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Container, Header } from './styles';
+import gravatar from 'gravatar';
+import useSWR, { useSWRInfinite } from 'swr';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { IDM } from '@typings/db';
 
 const DirectMessage = () => {
+  const { workspace, id } = useParams<{ workspace: string, id: string }>();
+
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR('/api/users', fetcher);
+  const { data: chatData, mutate: mutateChat, revalidate } = useSWR<IDM[]>(
+    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=$1`, fetcher
+  )
+
+  const [chat, onChangeChat, setChat] = useInput('');
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    
+    if (chat?.trim()) {
+      console.log(chat);
+      
+      axios.post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+        content: chat,
+      })
+        .then(() => {
+          revalidate();
+          setChat('');
+        })
+        .catch(console.error);
+    }
+  }, [chat]);
+
+  if (!userData || !myData ) {
+    return null;
+  }
+
   return (
-    <div>DM</div>
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d:'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat={""} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
+    </Container>
   )
 }
 
