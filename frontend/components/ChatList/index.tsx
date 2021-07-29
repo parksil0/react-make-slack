17e4ebf32fa923f9aaf8527b00e2737852979ff4
parksil0 <parksil00@naver.com>
2021-07-29 18:52:19 +0900

    채팅창 컴포넌트 추가  - 부연설명    - Chat는 ChatList안에 채팅 내용이고, ChatList는 Chat을 보여주는 컴포넌트    - 그래서 ChatList는 Chat을 보다 가독성있게 보여주기 위해 채팅 날짜, 커스텀 스크롤을 사용한다

--- frontend/components/ChatList/index.tsx
+++ frontend/components/ChatList/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import { ChatZone, Section } from './styles';
+import Chat from '@components/Chat';
+import { IDM } from '@typings/db';
+import React, { useCallback, useRef, VFC } from 'react';
+import { ChatZone, Section, StickyHeader } from './styles';
+import { Scrollbars } from 'react-custom-scrollbars'
 
-const ChatList = () => {
+interface Props {
+  chatSections: { [key: string]: IDM[] };
+}
+
+const ChatList: VFC<Props> = ({ chatSections }) => {
+  const scollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
   return (
     <ChatZone>
-      <Section>section</Section>
+      <Scrollbars autoHide ref={scollbarRef} onScrollFrame={onScroll} >
+        {Object.entries(chatSections).map(([date, chats]) => {
+          return (
+            <Section className={`section-${date}`} key={date} >
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+              {chats.map((chat) => {
+                <Chat key={chat.id} data={chat} />
+              })}
+            </Section>
+          )
+        })}
+      </Scrollbars>
     </ChatZone>
+    
   )
 }
 
