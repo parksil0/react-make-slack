8acf05b7dfa09686db5118ee2b92ab9589fa7a1b
parksil0 <parksil00@naver.com>
2021-07-31 22:39:36 +0900

    scrollRef 상위 컴포넌트로 이동, 상황에 따른 스크롤 이동(채팅 입력 시, 이전 대화 로딩 할 때)

--- frontend/components/ChatList/index.tsx
+++ frontend/components/ChatList/index.tsx
@@ -1,24 +1,31 @@
 import Chat from '@components/Chat';
 import { IDM } from '@typings/db';
-import React, { useCallback, useEffect, useRef, VFC } from 'react';
+import React, { forwardRef, RefObject, useCallback, useEffect, useRef, VFC } from 'react';
 import { ChatZone, Section, StickyHeader } from './styles';
 import { Scrollbars } from 'react-custom-scrollbars'
 
 interface Props {
   chatSections: { [key: string]: IDM[] };
+  setSize: (f: (index: number) => number) => Promise<IDM[][] | undefined>;
+  isReachingEnd: boolean;
+  scrollRef: RefObject<Scrollbars>
 }
 
-const ChatList: VFC<Props> = ({ chatSections }) => {
+const ChatList: VFC<Props> = ({ chatSections, setSize, isReachingEnd, scrollRef }) => {
   const scollbarRef = useRef(null);
   const onScroll = useCallback((values) => {
-    if (values.scrollTop === 0) {
+    if (values.scrollTop === 0 && !isReachingEnd) {
       console.log('최상단');
+      setSize((prevSize) => prevSize + 1)
+        .then(() => {
+          scrollRef.current?.scrollTop(scrollRef.current?.getScrollHeight() - values.scrollHeight)
+        })
     }
   }, []);
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scollbarRef} onScrollFrame={onScroll} >
+      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll} >
         {Object.entries(chatSections).map(([date, chats]) => {
           return (
             <Section className={`section-${date}`} key={date} >
